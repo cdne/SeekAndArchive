2a297580fa54041ebf915b041972294d7e0fa9ff
Claudiu <cduneata@gmail.com>
2020-06-27 19:14:24 +0300

    Add first solution

---
+++ FindFile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SeekAndArchive
+{
+    internal class FindFile
+    {
+        private readonly List<string> pathList;
+
+        public FindFile()
+        {
+            pathList = new List<string>();
+        }
+
+        /// <summary>
+        /// Add to list file paths which contains the provided file
+        /// </summary>
+        /// <param name="directory"> iterated directory</param>
+        /// <param name="file"></param>
+        public void SearchFilesInDirectory(string directory, string file)
+        {
+            try
+            {
+                foreach (var fileInDirectory in Directory.GetFiles(directory))
+                {
+                    if (fileInDirectory.Contains(file))
+                    {
+                        pathList.Add(fileInDirectory);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error when searching for file: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Search computer directories
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="file"></param>
+        public void SearchDirectory(string directory, string file)
+        {
+            try
+            {
+                foreach (var dir in Directory.GetDirectories(directory))
+                {
+                    SearchFilesInDirectory(dir, file);
+                    SearchDirectory(dir, file);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error when searching for directory: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Return list with file paths
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetPathList()
+        {
+            return pathList;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,16 @@
 
 namespace SeekAndArchive
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            FindFile fileFinder = new FindFile();
+            fileFinder.SearchDirectory(args[0], args[1]);
+            foreach (var item in fileFinder.GetPathList())
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
